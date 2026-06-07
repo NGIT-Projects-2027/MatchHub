25a643d36ce8d6c1a8b0dbc18218510667b25696
TNaveen-24 <naveenterukula@gmail.com>
2026-06-08 03:29:47 +0530

    Revert ?default=false as it breaks some valid images

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -46,7 +46,7 @@ export default function BookCard({
     image_url,
   } = book;
 
-  const finalImageUrl = image_url || (isbn ? `https://covers.openlibrary.org/b/isbn/${isbn}-L.jpg?default=false` : "");
+  const finalImageUrl = image_url || (isbn ? `https://covers.openlibrary.org/b/isbn/${isbn}-L.jpg` : "");
 
   return (
     <motion.div
